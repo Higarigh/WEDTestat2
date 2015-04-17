22c5774f8c7f2728409561275203c12cfad25f26
Philip Schmid <philip.schmid@outlook.com>
2015-04-17 10:12:32 +0200

    base

--- app.js
+++ app.js
@@ -9,6 +9,106 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var Link = require("./link.js");
+
+var links = [ ];
+
+app.get('/links', function(req, res) {
+    renderData(res, links);
+});
+
+app.get('/links/:id', function(req, res) {
+    renderData(res, links[Number(req.params.id)]);
+});
+
+app.post('/links/:id/up', function(req, res) {
+    var entityId = Number(req.params.id);
+    var data = links[entityId];
+    data.rating._up();
+
+
+});
+
+app.post('/links/:id/down', function(req, res) {
+    var entityId = Number(req.params.id);
+    var data = links[entityId];
+    console.log("blub");
+    console.log(entityId);
+
+    data.rating._down();
+});
+
+app.post('/links', function(req, res) {
+
+    readData(req, function(data) {
+        if (data) {
+            var entityId = links.length;
+            links[entityId] = data;
+            data.id = entityId;
+        }
+        renderData(res, data);
+    });
+
+});
+
+app.put('/links/:id', function(req, res) {
+    readData(req, function(data) {
+        if (data) {
+            var entityId = Number(req.params.id);
+            if (links[entityId]) {
+                links[entityId] = data;
+                data.id = entityId;
+            }
+        }
+        renderData(res, data);
+    });
+});
+
+app.delete('/links/:id', function(req, res) {
+    var entityId = Number(req.params.id);
+    var data = links[entityId];
+    links.splice(entityId, 1);
+    renderData(res, entityId);
+});
+
+function readData(req, callback) {
+    var body = '';
+    req.on(
+        'readable' ,
+        function () {
+            var rawBody = req.read();
+            if (rawBody) {
+                if (typeof rawBody === 'string') {
+                    body += rawBody;
+                } else if (typeof rawBody === 'object' && rawBody instanceof Buffer) {
+                    body += rawBody.toString('utf8');
+                }
+            }
+        });
+    req.on(
+        'end',
+        function() {
+            callback(body ? JSON.parse(body) : null);
+        });
+}
+
+function renderData(res, data) {
+    res.writeHead(200, {
+        "Content-Type" : "application/json"
+    });
+
+    res.end(JSON.stringify({
+        data : data || null
+    }));
+}
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,37 +124,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
